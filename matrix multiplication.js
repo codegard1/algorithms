7ebf4cb20c2d35b6734ac6bccb2e1080a02a6d25
Chris Odegard <c.odegard@gmail.com>
2018-07-24 23:02:42 -0400

    working algorithm but only for isometric matrices

--- matrix multiplication.js
+++ matrix multiplication.js
@@ -1,32 +1,94 @@
-var p = []; for (var i = 0; i < 3; i++) { p[i] = []; for (var j = 0; j < 3; j++) { p[i][j] = Math.floor(Math.random() * 100) } }
-var q = []; for (var i = 0; i < 3; i++) { q[i] = []; for (var j = 0; j < 3; j++) { q[i][j] = Math.floor(Math.random() * 100) } }
+class matrix {
+  constructor(cols, rows, multiplier, name) {
+    var m = [];
+    for (var i = 0; i < rows; i++) {
+      m[i] = [];
+      for (var j = 0; j < cols; j++) {
+        m[i][j] = Math.floor(Math.random() * multiplier);
+      }
+    }
 
-/* Element r[i,j] is the dot product of the ith row of p with the jth column of q */
-/* The dot product is simply the sum of the N term-by-term multiplications p[i,1]*q[1,j]+p[i,2]*q[i,j]+...p[i,N-1]*q[N-1,j] */
+    this.value = m;
+    this.rows = rows;
+    this.cols = cols;
+    this.multiplier = multiplier;
+    this.name = name || "";
 
-function multiply(p, q) {
-    var pCol = p[0].length;
-    console.log('p columns: ' + pCol);
-    console.log('p rows: ' + p.length);
-    console.log('q columns: ' + q[0].length);
-    var qRows = q.length;
-    console.log('q rows: ' + qRows);
+    return this;
+  }
+
+  print() {
+    console.log("matrix: " + this.name);
+    var m = this.value;
+
+    for (var i = 0; i < this.cols; i++) {
+      console.log(m[i]);
+    }
+  }
+
+  setFromArray(arr, name) {
+    this.name = name || this.name;
+    this.value = arr || [];
+    this.rows = arr ? arr.length : undefined;
+    this.cols = arr[0] ? arr[0].length : undefined;
+  }
+
+  set(i, j, n) {
+    this.value[i][j] = n;
+  }
+
+  get(i, j) {
+    return this.value[i][j];
+  }
+
+  /* Element r[i,j] is the dot product of the ith row of p with the jth column of q */
+  /* The dot product is simply the sum of the N term-by-term multiplications p[i,1]*q[1,j]+p[i,2]*q[i,j]+...p[i,N-1]*q[N-1,j] */
+
+  multiply(mtx) {
+    var p = this,
+      q = mtx;
+    var pCol = p.cols;
+    var qRows = q.rows;
+    p.print();
+    q.print();
 
     // result matrix
-    var r = [];
+    var r = new matrix(3, 3, 0, "result");
 
     //  if p.columns == q.rows
     if (pCol === qRows) {
-        // For each row in matrix p 
-        for (var i = 0; i < p.length; i++) {
-            // For each column in matrix q
-            for (j = 0; j < p.length; j++) {
-                // console.log(i + '.' + j);
-                var n = p[i][j];
-                var m = q[j][i];
-                console.log(n + 'x' + m + '=' + (n * m));
-                r[i].push(p[i][j] * q[j][i]);
-            }
+      // For each row in matrix p
+      for (var i = 0; i < pCol; i++) {
+        // For each column in matrix q
+        for (var j = 0; j < qRows; j++) {
+          // console.log("...");
+
+          var t = 0;
+          for (var k = 0; k < pCol; k++) {
+            var n = p.get(i, k);
+            var m = q.get(k, j);
+            t += n * m;
+            //   console.log(n + "x" + m + "=" + n * m + "  t:" + t);
+          }
+
+          r.set(i, j, t);
         }
+      }
     }
+
+    return r;
+  }
 }
+
+console.time("matrix-multiplication.js");
+// Generate Sample matrices
+var p = new matrix(3, 3, 100, "p");
+var q = new matrix(3, 3, 100, "q");
+
+// generate a result
+var r = p.multiply(q);
+
+// Print the result
+r.print();
+
+console.timeEnd("matrix-multiplication.js");
