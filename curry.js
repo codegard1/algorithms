5c2f241ecbcbf454eedf9cf1907ffa252bbda5e6
Chris Odegard <c.odegard@gmail.com>
2018-08-14 20:38:56 -0400

    added curry and fizzbuzz

---
+++ curry.js
@@ -0,0 +1,25 @@
+/**
+ * Basic Currying example
+ * A curried function that takes multiple paramaters decomposes into a series of unary functions that each returns a function with a single parameter
+ * This is possible because the bottmmost closure has access to all parameters passed to the topmost function
+ */
+
+// Normal function
+function additionNormal(x, y) {
+  return x + y;
+}
+
+// Curried function
+function additionCurried(x) {
+  return function(y) {
+    return x + y;
+  };
+}
+
+function curry(f) {
+  return function(x) {
+    return function(y) {
+      return f(x, y);
+    };
+  };
+}
