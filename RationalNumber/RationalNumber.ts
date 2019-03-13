17d9fa0af8e9ca7c66df124239cfeaf514e1c6c3
Chris Odegard <c.odegard@gmail.com>
2019-03-13 18:21:40 -0400

    Add math functions for use with RationalNumber class

--- RationalNumber/RationalNumber.ts
+++ RationalNumber/RationalNumber.ts
@@ -23,7 +23,66 @@ class RationalNumber {
     return this.denom;
   }
 
+
+  // public cons(x: number, y: number) {
+  //   function dispatch(m: number): number {
+  //     if (m === 0) { return x }
+  //     else if (m === 1) { return y }
+  //     else { console.error('Arguemtn not 0 or 1', m); }
+  //   }
+  //   return dispatch;
+  // }
+
   public toArray(): Array<number> {
     return [this.car(), this.cdr()];
   }
+
+  public print(): string {
+    return `${this.numer} / ${this.denom}`;
+  }
+
+  public equals(m: RationalNumber): boolean {
+    return ((this.numer * m.denom) === (m.numer * this.denom));
+  }
 }
+
+function addRat(x: RationalNumber, y: RationalNumber): RationalNumber {
+  return new RationalNumber(
+    (x.numer * y.denom) + (y.numer * x.denom),
+    (x.denom * y.denom)
+  )
+}
+
+function subRat(x: RationalNumber, y: RationalNumber): RationalNumber {
+  return new RationalNumber(
+    (x.numer * y.denom) - (y.numer * x.denom),
+    (x.denom * y.denom)
+  )
+}
+
+function multiRat(x: RationalNumber, y: RationalNumber): RationalNumber {
+  return new RationalNumber(
+    (x.numer * y.numer),
+    (x.denom * y.denom)
+  )
+}
+
+function divRat(x: RationalNumber, y: RationalNumber): RationalNumber {
+  return new RationalNumber(
+    (x.numer * y.denom),
+    (x.denom * y.numer)
+  )
+}
+
+!function (): boolean {
+  const x = new RationalNumber(1, 2);
+  // console.log(x.print());
+
+  const y = new RationalNumber(3, 4);
+  // console.log(y.print());
+
+  const r = addRat(x, y);
+  console.log(r.print()); // 10 / 8
+
+  return true;
+}();
